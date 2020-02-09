33271956f86d9fc53d76cc4d90a09d9fad507d33
GiselleTodd <43821673+GiselleTodd@users.noreply.github.com>
2020-02-09 14:20:53 -0500

    complete profile

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import date
 
 ###
 # Routing for your application.
@@ -23,6 +23,15 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route("/profile")
+def profile():
+    join = format_date_joined(date.today())
+    return render_template('profile.html', date=join)
+
+#This function formats the date joined to present the Month and Year
+def format_date_joined(date_joined):
+    return date_joined.strftime("%B, %Y")
 
 
 ###
@@ -52,6 +61,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+    
 
 
 if __name__ == '__main__':
